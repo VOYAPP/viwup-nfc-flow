73630b69997e93a00fedf18741516d2dbc6328cc
VOYAPP <stormbrainmoney@gmail.com>
2026-08-09 12:28:30 -0400

    Add configuration file with initial settings

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  primaryColor: '#1B7A6E',
+  logoUrl: null,
+  localName: 'Restaurante Demo',
+  garzones: ['Andrés', 'Camila', 'Diego', 'Valentina'],
+  googleMapsUrl: 'https://maps.google.com',
+  webhookUrl: null,
+};
