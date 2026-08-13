4815652d8f6bb237b658c733ff1d629091834334
VOYAPP <stormbrainmoney@gmail.com>
2026-08-13 19:09:48 -0400

    Refactor CONFIG to CLIENTES object structure

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
-var CONFIG = {
-  idLocal: 'REST_VIWUP_01',
-  localName: 'Restaurante Demo',
-  webhookUrl: 'https://hook.us2.make.com/2f2amy2uq88ptk4fksltadyxn485xeov',
-  primaryColor: '#1B7A6E',
-  logoUrl: null,
-  garzones: ['Andrés', 'Camila', 'Diego', 'Valentina'],
-  motivos: ['Servicio', 'Cocina', 'Barra'],
-  googleMapsUrl: 'https://www.google.com/maps'
+var CLIENTES = {
+  "demo": {
+    idLocal: 'REST_VIWUP_01',
+    localName: 'Restaurante Demo',
+    webhookUrl: 'https://hook.us2.make.com/2f2amy2uq88ptk4fksltadyxn485xeov',
+    primaryColor: '#1B7A6E',
+    logoUrl: null,
+    garzones: ['Andrés', 'Camila', 'Diego', 'Valentina'],
+    motivos: ['Servicio', 'Cocina', 'Barra'],
+    googleMapsUrl: 'https://www.google.com/maps'
+  },
+  "lucianosbbq": {
+    idLocal: 'REST_LUCIANOS_01',
+    localName: 'Luciano\'s BBQ',
+    webhookUrl: 'https://hook.us2.make.com/2f2amy2uq88ptk4fksltadyxn485xeov',
+    primaryColor: '#B71C1C',
+    logoUrl: null,
+    garzones: ['Carlos', 'Ana', 'Luis', 'Sofía'],
+    motivos: ['Carne fría', 'Demora', 'Servicio', 'Limpieza'],
+    googleMapsUrl: 'https://www.google.com/maps/place/...' 
+  }
+  // Para agregar el cliente 3, solo pones una coma arriba y pegas otro bloque
 };
